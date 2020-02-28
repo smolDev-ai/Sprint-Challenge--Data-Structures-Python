cf6f4a2df11d4b8a3b686b6b45b0f17e30f79e8a
Megan <megan.miller@smoldev.dev>
2020-02-28 10:38:43 -0600

    names.py done time down to 0.6 seconds

--- names/names.py
+++ names/names.py
@@ -1,4 +1,5 @@
 import time
+from binary_search_tree import BinarySearchTree
 
 start_time = time.time()
 
@@ -10,13 +11,18 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-duplicates = []  # Return the list of duplicates in this data structure
-
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# Runtime of this is O(n) or O(n^2) -- That said, the files both contain 10000
+# items so I'm a little hesistant. My best guess is O(n).
+bst = BinarySearchTree(names_1[0])
+names = 1
+
+while names <= len(names_1[1:]):
+    bst.insert(names_1[names])
+    names += 1
+
+
+duplicates = [duplicate for duplicate in names_2 if bst.contains(duplicate)]
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
